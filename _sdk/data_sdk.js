eb7495d6303a4193f9f12f68631cae7244ce4963
atikibunenenes-eng <atikibunenenes@gmail.com>
2025-12-31 17:56:01 +0700

    Add files via upload

---
+++ _sdk/data_sdk.js
@@ -0,0 +1,1 @@
+console.warn("data_sdk.js is missing. Please restore this file for the application to function correctly.");
---
+++ _sdk/element_sdk.js
@@ -0,0 +1,1 @@
+console.warn("element_sdk.js is missing. Please restore this file for the application to function correctly.");
